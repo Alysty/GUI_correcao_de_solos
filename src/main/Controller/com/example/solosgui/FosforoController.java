4587dbc93e2c6b95640c5a438b677ef3f36f0fd1
Pedro Jorge <pedro.paulo@gubee.com.br>
2021-10-28 00:00:49 -0300

    Added fxId to all fields that will be used and its controller couterpart

--- src/main/Controller/com/example/solosgui/FosforoController.java
+++ src/main/Controller/com/example/solosgui/FosforoController.java
@@ -1,4 +1,14 @@
 package com.example.solosgui;
 
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class FosforoController {
+    public TextField teorFosforo;
+    public TextField fonteFosforo;
+    public TextField eficienciaFosforo;
+    public Text custoTotal;
+    public Text quantidadeAplicar;
+    public Text primeiroNutrienteAdicional;
+    public Text segundoNutrienteAdicional;
 }
--- src/main/Controller/com/example/solosgui/PotassioController.java
+++ src/main/Controller/com/example/solosgui/PotassioController.java
@@ -1,5 +1,19 @@
 package com.example.solosgui;
 
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class PotassioController {
 
+    public TextField fonteCorretivo;
+    public TextField calcioCTCDesejada;
+    public TextField pRNT;
+    public TextField teorCaOCorretivo;
+    public Text custoTotal;
+    public Text potassioCTCAtual;
+    public Text primeiroNutrienteAdicional;
+    public Text segundoNutrienteAdicional;
+    public Text quantidadeAplicar;
+    public Text vPorcentagemAtual;
+    public Text vPorcentagemAosCorrecoes;
 }
